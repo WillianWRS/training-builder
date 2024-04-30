97408070f67b5440ce40f83852991220141a00e1
WILLIAN\willi <vsm.willian@gmail.com>
2024-04-29 23:03:28 -0300

    Commit inicial

---
+++ training-builder/src/main/java/com/trainingbuilder/TrainingBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.trainingbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingBuilderApplication.class, args);
+	}
+
+}
---
+++ training-builder/src/test/java/com/trainingbuilder/TrainingBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainingbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
